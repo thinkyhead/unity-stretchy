e335798ceceafc3a7b8e274b2c24d4e4db58c5d4
Scott Lahteine <sourcetree@thinkyhead.com>
2015-11-23 19:04:47 -0800

    Add Scripts

---
+++ Assets/Scripts/OrbitZoomCam.cs
@@ -0,0 +1,149 @@
+﻿/**
+ * OrbitZoomCam.cs
+ *
+ * The most basic of orbiting-zooming cameras...
+ *
+ * - Orbit the camera by dragging with the finger or mouse.
+ * - Zoom the camera with pinch, mousewheel, or scroll gesture.
+ *
+ */
+using UnityEngine;
+using System.Collections;
+
+public class OrbitZoomCam : MonoBehaviour {
+
+  public static OrbitZoomCam instance;
+  public float inertialOrbit = 0, dampenOrbit = 0;
+
+  public Vector3 target = new Vector3(0, 500, 0);
+  float distance = 15, newDist = 15;
+  float xSpeed = 25, ySpeed = 12;
+  float distanceMin = 10, distanceMax = 100;
+ 
+  int firstFingerID = -1, pinchFingerID = -1;
+  Vector2 firstFingerXY, pinchFingerXY;
+  float prevPinchDist = 0, zoomDelta = 0;
+  Vector2 orbitVelocity = new Vector2(0, 0);
+  float orbitFactor;
+
+  Transform T;
+
+  void Awake() {
+    instance = this;
+    orbitFactor = 1 - dampenOrbit;
+    T = transform;
+  }
+  
+  void Start() {
+    newDist = distance;
+
+    Vector3 angles = T.eulerAngles;
+    T.rotation = Quaternion.Euler(angles.y, angles.x, 0);
+    T.position = new Vector3(0.0f, 0.0f, -distance) + target;
+
+    // Make the rigid body not change rotation
+    Rigidbody r = GetComponent<Rigidbody>();
+    if (r != null) r.freezeRotation = true;
+  }
+  
+  void Update() {
+    zoomDelta = -Input.GetAxis("Mouse ScrollWheel");
+    Vector2 rotDelta = Vector2.zero;
+    
+    // Handle touches, if there are any
+    if (Input.touchCount > 0) {
+      Vector2 firstFingerHV = Vector2.zero;
+      foreach (Touch t in Input.touches) {    
+        // Debug.Log("Touches exist and rotDelta is  " + rotDelta);
+        int fid = t.fingerId;
+        Vector2 pos = t.position;
+        switch (t.phase) {
+          case TouchPhase.Began:
+            if (firstFingerID == -1) {
+              firstFingerID = fid;
+              firstFingerXY = pos;
+              firstFingerHV.Set(0, 0);
+              // Debug.Log("Touch began at "+pos);
+            }
+            else if (pinchFingerID == -1) {
+              pinchFingerID = fid;
+              pinchFingerXY = pos;
+              prevPinchDist = (firstFingerXY - pinchFingerXY).magnitude;
+            }
+            break;
+
+          case TouchPhase.Stationary:
+            if (firstFingerID == fid) {
+              // Debug.Log("Touch stationary at "+pos);
+              firstFingerHV.Set(0, 0);
+            }
+            else if (pinchFingerID == fid) {
+            }
+            break;
+          case TouchPhase.Moved:
+            if (firstFingerID == fid) {
+              // Debug.Log("Touch moved at " + pos + " with first at " + firstFingerXY);
+              firstFingerHV = pos - firstFingerXY;
+              firstFingerXY = pos;
+            }
+            else if (pinchFingerID == fid) {
+              pinchFingerXY = pos;
+            }
+            break;
+
+          case TouchPhase.Ended:
+          case TouchPhase.Canceled:
+            if (firstFingerID == fid) {
+              // Debug.Log("Touch ended at " + pos + " with previous at " + firstFingerXY + " and pinch at " + pinchFingerXY);
+              firstFingerID = pinchFingerID;
+              firstFingerXY = pinchFingerXY;
+              firstFingerHV = Vector2.zero;
+              pinchFingerID = -1;
+            }
+            else if (pinchFingerID == fid) {
+              pinchFingerID = -1;
+            }
+            break;
+        }
+      }
+  
+      // if pinching is still happening...
+      if (pinchFingerID != -1) {
+        // zoom by the difference since last time
+        float dist = (firstFingerXY - pinchFingerXY).magnitude;
+        zoomDelta = (dist - prevPinchDist) * 0.01f;
+        prevPinchDist = dist;
+      }
+      else if (firstFingerHV.x != 0 || firstFingerHV.y != 0) {
+        // Debug.Log("Rotation changed by " + firstFingerHV);
+        rotDelta = firstFingerHV * 0.2f;
+        orbitVelocity = rotDelta * inertialOrbit;
+      }
+    }
+    // only get the mouse button if there are no touches
+    else if (Input.GetMouseButton(0)) {
+      rotDelta.Set(0.02f * xSpeed * Input.GetAxis("Mouse X"), 0.02f * ySpeed * Input.GetAxis("Mouse Y"));
+      orbitVelocity = rotDelta * inertialOrbit;
+    }
+
+    if (rotDelta != Vector2.zero) {
+      T.rotation *= Quaternion.AngleAxis(rotDelta.x, Vector3.up) * Quaternion.AngleAxis(rotDelta.y, Vector3.left);
+    }
+    else if (inertialOrbit > 0 && orbitVelocity != Vector2.zero) {
+      T.rotation *= Quaternion.AngleAxis(orbitVelocity.x, Vector3.up) * Quaternion.AngleAxis(orbitVelocity.y, Vector3.left);
+      orbitVelocity *= orbitFactor;
+    }
+
+    if (zoomDelta != 0) {
+      newDist -= zoomDelta * Mathf.Abs(newDist / 2);
+      newDist = Mathf.Clamp(newDist, distanceMin, distanceMax);
+    }
+
+    float howfar = newDist - distance;
+    if (Mathf.Abs(howfar) > 5) {
+      distance += howfar / 5;
+    }
+
+    T.position = target + (T.rotation * new Vector3(0.0f, 0.0f, -distance));
+  }
+}
---
+++ Assets/Scripts/StretchyTethered.cs
@@ -0,0 +1,279 @@
+﻿/**
+ * StretchyTethered.cs
+ *
+ * Stretch an object (on its Z axis) between two Transforms.
+ *
+ * If the Transform target for an end is unset, the end remains tethered
+ * to the last global point that was set.
+ *
+ */
+using UnityEngine;
+using System.Collections;
+
+//[NoExecuteInEditMode]
+public class StretchyTethered : Stretchy {
+
+  // Let Targets have properties instead of using multiple arrays
+  public StretchyTarget[] target = new StretchyTarget[2];
+
+  /**
+   * When starting out, offsets will be assigned based on the
+   * current distances from the ends to their targets.
+   * This means the Stretchy object needs to be pre-scaled so its
+   * ends align with the target objects.
+   */
+  protected override void Start() {
+    base.Start();
+
+    // Get the current offsets and preserve them
+    RefreshTargetOffsets();
+
+    // Get the default end distance margins from the targets
+    ResetTargetMargins();
+  }
+
+  /**
+   * Set target points to object world positions plus offsets.
+   * The local offset is always applied. The world offset is applied if enabled.
+   * We call Stretchy.Update to do the hard work.
+   */
+  protected override void Update() {
+    for (int i = 0; i < 2; i++) {
+      StretchyTarget t = target[i];
+      if (t != null) targetPoint[i] = t.worldTetherPoint;
+    }
+
+    base.Update();
+  }
+
+  /**
+   * Reset targetMargin values from each target's defaultMargin
+   * If an end has no target, leave it alone.
+   */
+  void ResetTargetMargins() {
+    for (int i = 0; i < 2; i++) {
+      StretchyTarget t = target[i];
+      if (t != null) targetMargin[i] = t.defaultMargin;
+      // targetMargin[i] = (t != null) ? t.defaultMargin : 0;
+    }
+  }
+
+  /**
+   * Set target.LocalOffset vectors for both ends based on current distance from targets.
+   */
+  public void RefreshTargetLocalOffsets() {
+    target[0].useWorldOffset = target[1].useWorldOffset = false;
+    RefreshTargetOffsets();
+  }
+
+  /**
+   * Set target.LocalOffset vectors for both ends based on current distance from targets.
+   */
+  public void RefreshTargetWorldOffsets() {
+    target[0].useWorldOffset = target[1].useWorldOffset = true;
+    RefreshTargetOffsets();
+  }
+
+  /**
+   * Using the current arrangement of objects,
+   * set the target.localOffset and target.worldOffset vectors for both ends.
+   *
+   * The ends may be swapped if needed.
+   *
+   * Currently the ends are both set as having either local or world offsets
+   * on initialization. World offsets use slightly less processing, but
+   * local offsets are the default because they are the more common case,
+   * working properly even as the system rotates and scales in the world.
+   */
+  public void RefreshTargetOffsets() {
+
+    int targetCount = 0;
+    bool[] hasTarget = new bool[2];
+    for (int i = 0; i < 2; i++)
+      if (hasTarget[i] = (target[i] != null)) targetCount++;
+
+    if (targetCount == 0) return;
+
+    Vector3[] endPt = endPoints; // Get start and end points of the stretch
+
+    if (targetCount == 1) {
+
+      // With 1 target a reversal means the ends will be preset as:
+      //  (A) Associated with a target, but the point farther from the target
+      //  (B) Associated with no target, but the point closer to the target
+      //
+      // The correct solution is to swap the tether-points:
+      //  (A) Associated with a target and the point closer to the target
+      //  (B) Associated with no target and the point farther from the target
+
+      int targetEnd = hasTarget[0] ? 0 : 1;         // The index with a target
+      StretchyTarget t = target[targetEnd];         // The target component
+
+      // Is the end with no target actually closer to the target?
+      if (t.WorldDistanceToPoint(endPt[1-targetEnd]) < t.WorldDistanceToPoint(endPt[targetEnd])) {
+        SwapTargetPoints();
+        endPt.Swap();
+      }
+    }
+    else { // targetCount == 2
+
+      // End 1 closer to target 2? Just swap the points to get smallest offsets.
+      if (target[1].WorldDistanceToPoint(endPt[0]) < target[0].WorldDistanceToPoint(endPt[0]))
+        endPt.Swap();
+    }
+
+    for (int i = 0; i < 2; i++) {
+      if (hasTarget[i]) {
+        StretchyTarget t = target[i];
+        if (t.useWorldOffset)
+          t.SetOffsets(Vector3.zero, t.WorldOffsetToPoint(endPt[i]));
+        else
+          t.SetOffsets(t.LocalOffsetToPoint(endPt[i]), Vector3.zero);
+      }
+    }
+
+  } // end of RefreshTargetOffsets
+
+  /**
+   * Set an end's target local-offset based on current relative positions.
+   * Use this to update an offset later.
+   */
+  public void RefreshTargetLocalOffset(int end) {
+    target[end].useWorldOffset = false;
+    RefreshTargetOffset(end);
+  }
+
+  /**
+   * Set an end's target world-offset based on current relative positions.
+   * Use this to update an offset later.
+   */
+  public void RefreshTargetWorldOffset(int end) {
+    target[end].useWorldOffset = true;
+    RefreshTargetOffset(end);
+  }
+
+  /**
+   * Set an end's local or world target offset based on current relative positions.
+   * In this implementation, setting one clears the other. But technically it's no
+   * problem to use both in tandem.
+   */
+  public void RefreshTargetOffset(int end) {
+    if (end == 0 || end == 1) {
+      StretchyTarget t = target[end];
+      if (t != null) {
+        if (t.useWorldOffset)
+          t.SetOffsets(Vector3.zero, t.WorldOffsetToPoint(endPoints[end]));
+        else
+          t.SetOffsets(t.LocalOffsetToPoint(endPoints[end]), Vector3.zero);
+      }
+    }
+  }
+
+  /**
+   * Tether one of the ends to a StretchyTarget (null is ok)
+   */
+  public void TetherEndToTarget(int end, StretchyTarget target) {
+    TetherEndToTargetWithOffset(end, target, Vector3.zero);
+  }
+
+  /**
+   * Tether one of the ends to a Transform (null is ok)
+   */
+  public void TetherEndToTransform(int end, Transform trans) {
+    TetherEndToTransformWithOffset(end, trans, Vector3.zero);
+  }
+
+  /**
+   * Tether one of the ends to a StretchyTarget (null is ok) with a world offset
+   */
+  public void TetherEndToTargetWithWorldOffset(int end, StretchyTarget target, Vector3 offset) {
+    TetherEndToTargetWithOffset(end, target, offset, true);
+  }
+
+  /**
+   * Tether one of the ends to a Transform (null is ok) with a world offset
+   */
+  public void TetherEndToTransformWithWorldOffset(int end, Transform trans, Vector3 offset) {
+    TetherEndToTransformWithOffset(end, trans, offset, true);
+  }
+
+  /**
+   * Tether one of the ends to a StretchyTarget (null is ok) with a local offset
+   */
+  public void TetherEndToTargetWithLocalOffset(int end, StretchyTarget target, Vector3 offset) {
+    TetherEndToTargetWithOffset(end, target, offset, false);
+  }
+
+  /**
+   * Tether one of the ends to a Transform (null is ok) with a local offset
+   */
+  public void TetherEndToTransformWithLocalOffset(int end, Transform trans, Vector3 offset) {
+    TetherEndToTransformWithOffset(end, trans, offset, false);
+  }
+
+  /**
+   * Tether one of the ends to a Transform (null is ok) with a local or world offset
+   * The other offset is set to zero, but technically both can be used together
+   */
+  public void TetherEndToTransformWithOffset(int end, Transform trans, Vector3 offset, bool isWorld=false) {
+    GameObject o = trans.gameObject;
+    StretchyTarget t = o.GetComponent<StretchyTarget>();
+    if (t == null) t = o.AddComponent<StretchyTarget>();
+    TetherEndToTargetWithOffset(end, t, offset, isWorld);
+  }
+
+  /**
+   * Tether one of the ends to a StretchyTarget (null is ok) with a local or world offset
+   * The other offset is set to zero, but technically both can be used together
+   */
+  public void TetherEndToTargetWithOffset(int end, StretchyTarget t, Vector3 offset, bool isWorld=false) {
+    if (end == 0 || end == 1) {
+      if (t != null) {
+        if (isWorld)
+          t.SetOffsets(Vector3.zero, offset);
+        else
+          t.SetOffsets(offset, Vector3.zero);
+      }
+      target[end] = t;
+    }
+  }
+
+  /**
+   * Untether one of the ends, leaving it tethered to its current position
+   */
+  public void Untether(int end=-1) {
+    if (end == -1) {
+      TetherEndToTarget(0, null);
+      end = 1;
+    }
+    TetherEndToTarget(end, null);
+  }
+
+  /**
+   * Tether one of the ends to a global Point
+   */
+  public override void TetherEndToWorldPoint(int end, Vector3 point) {
+    if (end == 0 || end == 1) {
+      Untether(end);
+      base.TetherEndToWorldPoint(end, point);
+    }
+  }
+
+  /**
+   * Swap target and targetMargin, but not targetPoint
+   *
+   * One Target:
+   *  End A now tracks the target.
+   *  End B now tracks its last point.
+   */
+  public void SwapTargetObjects() {
+    target.Swap();
+    targetMargin.Swap();
+  }
+
+  public override void SwapTargets() {
+    base.SwapTargets();
+    SwapTargetObjects();
+  }
+
+} // end of StretchyTethered class
