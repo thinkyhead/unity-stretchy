19ae7341b7f7d47ecc104aac7a8171c6e7837784
Scott Lahteine <sourcetree@thinkyhead.com>
2015-11-23 20:21:23 -0800

    Demo code from Part 1
    http://www.thinkyhead.com/blogs/thinkyhead/2015-11-18/stretchy-objects-u
    nity-3d

--- Assets/Scripts/StretchyTethered.cs
+++ Assets/Scripts/StretchyTethered.cs
@@ -13,8 +13,14 @@ using System.Collections;
 //[NoExecuteInEditMode]
 public class StretchyTethered : Stretchy {
 
-  // Let Targets have properties instead of using multiple arrays
-  public StretchyTarget[] target = new StretchyTarget[2];
+  // Attach in editor or set manually
+  public Transform[] targetObj = new Transform[2];
+
+  // Offsets from each target in world space, to anchor to points near targets.
+  public Vector3[] targetWorldOffset = new Vector3[2];
+
+  // Offsets from each target in local space, which scale and rotate with targets.
+  public Vector3[] targetLocalOffset = new Vector3[2];
 
   /**
    * When starting out, offsets will be assigned based on the
@@ -27,214 +33,126 @@ public class StretchyTethered : Stretchy {
 
     // Get the current offsets and preserve them
     RefreshTargetOffsets();
-
-    // Get the default end distance margins from the targets
-    ResetTargetMargins();
   }
 
   /**
-   * Set target points to object world positions plus offsets.
-   * The local offset is always applied. The world offset is applied if enabled.
-   * We call Stretchy.Update to do the hard work.
+   * Set target points to object world positions plus offsets
+   * then call Stretchy.Update to do the hard work.
    */
   protected override void Update() {
-    for (int i = 0; i < 2; i++) {
-      StretchyTarget t = target[i];
-      if (t != null) targetPoint[i] = t.worldTetherPoint;
-    }
+    for (int i = 0; i < 2; i++)
+      if (targetObj[i] != null) {
+        Vector3 transformedOffset = targetObj[i].position - targetObj[i].TransformPoint(targetLocalOffset[i]);
+        // Debug.Log("Converting local: " + targetLocalOffset[i] + " to world: " + transformedOffset + " for " + targetObj[i].name);
+        targetPoint[i] = targetObj[i].position + targetWorldOffset[i] + transformedOffset;
+      }
 
     base.Update();
   }
 
   /**
-   * Reset targetMargin values from each target's defaultMargin
-   * If an end has no target, leave it alone.
-   */
-  void ResetTargetMargins() {
-    for (int i = 0; i < 2; i++) {
-      StretchyTarget t = target[i];
-      if (t != null) targetMargin[i] = t.defaultMargin;
-      // targetMargin[i] = (t != null) ? t.defaultMargin : 0;
-    }
-  }
-
-  /**
-   * Set target.LocalOffset vectors for both ends based on current distance from targets.
-   */
-  public void RefreshTargetLocalOffsets() {
-    target[0].useWorldOffset = target[1].useWorldOffset = false;
-    RefreshTargetOffsets();
-  }
-
-  /**
-   * Set target.LocalOffset vectors for both ends based on current distance from targets.
-   */
-  public void RefreshTargetWorldOffsets() {
-    target[0].useWorldOffset = target[1].useWorldOffset = true;
-    RefreshTargetOffsets();
-  }
-
-  /**
    * Using the current arrangement of objects,
-   * set the target.localOffset and target.worldOffset vectors for both ends.
-   *
-   * The ends may be swapped if needed.
-   *
-   * Currently the ends are both set as having either local or world offsets
-   * on initialization. World offsets use slightly less processing, but
-   * local offsets are the default because they are the more common case,
-   * working properly even as the system rotates and scales in the world.
+   * set the targetWorldOffset vectors for both ends.
+   * The targets may be swapped if needed.
    */
   public void RefreshTargetOffsets() {
 
     int targetCount = 0;
     bool[] hasTarget = new bool[2];
     for (int i = 0; i < 2; i++)
-      if (hasTarget[i] = (target[i] != null)) targetCount++;
+      if (hasTarget[i] = (targetObj[i] != null)) targetCount++;
 
     if (targetCount == 0) return;
 
-    Vector3[] endPt = endPoints; // Get start and end points of the stretch
+    Vector3[] endPt = endPoints, // Get start and end points of the stretch
+              targetWorldPos = new Vector3[2];
 
     if (targetCount == 1) {
 
       // With 1 target a reversal means the ends will be preset as:
-      //  (A) Associated with a target, but the point farther from the target
-      //  (B) Associated with no target, but the point closer to the target
+      //  (A) Associated with a target, but the end-point farther from the target
+      //  (B) Associated with no target, but the end-point closer to the target is set
       //
       // The correct solution is to swap the tether-points:
-      //  (A) Associated with a target and the point closer to the target
-      //  (B) Associated with no target and the point farther from the target
+      //  (A) Associated with a target and the end-point closer to the target
+      //  (B) Associated with no target and the end-point farther from the target
 
-      int targetEnd = hasTarget[0] ? 0 : 1;         // The index with a target
-      StretchyTarget t = target[targetEnd];         // The target component
+      int tetherEnd = hasTarget[0] ? 0 : 1,               // The index that has a target
+          otherEnd = 1 - tetherEnd;
+      Vector3 targetPos = targetObj[tetherEnd].position;  // The world position of the target
 
       // Is the end with no target actually closer to the target?
-      if (t.WorldDistanceToPoint(endPt[1-targetEnd]) < t.WorldDistanceToPoint(endPt[targetEnd])) {
+      if ((endPt[otherEnd] - targetPos).magnitude < (endPt[tetherEnd] - targetPos).magnitude) {
         SwapTargetPoints();
         endPt.Swap();
       }
+
+      targetWorldPos[tetherEnd] = targetPos;
+      targetWorldPos[otherEnd] = endPt[otherEnd];
+
     }
     else { // targetCount == 2
 
-      // End 1 closer to target 2? Just swap the points to get smallest offsets.
-      if (target[1].WorldDistanceToPoint(endPt[0]) < target[0].WorldDistanceToPoint(endPt[0]))
-        endPt.Swap();
+      // With 2 targets a reversal means the ends will be preset as:
+      //  (A) Associated with a target, but the end-point closer to the other target
+      //  (B) Associated with a target, but the end-point closer to the other target
+      //
+      // The correct solution is to keep the targets but swap the offsets:
+      //  (A) Associated with the same target, but the end-point closer to it
+      //  (A) Associated with the same target, but the end-point closer to it
+
+      float[] firstEndDistance = new float[2];
+
+      for (int i = 0; i < 2; i++) {
+        targetWorldPos[i] = targetObj[i].position;
+        firstEndDistance[i] = (endPt[0] - targetWorldPos[i]).magnitude;
+      }
+
+      // If the first end is closer to the second target, swap global positions
+      if (firstEndDistance[1] < firstEndDistance[0]) endPt.Swap();
     }
 
     for (int i = 0; i < 2; i++) {
-      if (hasTarget[i]) {
-        StretchyTarget t = target[i];
-        if (t.useWorldOffset)
-          t.SetOffsets(Vector3.zero, t.WorldOffsetToPoint(endPt[i]));
-        else
-          t.SetOffsets(t.LocalOffsetToPoint(endPt[i]), Vector3.zero);
-      }
+      Vector3 worldOffs = endPt[i] - targetWorldPos[i];
+      // targetWorldOffset[i] = worldOffs;
+      targetLocalOffset[i] = hasTarget[i] ? -targetObj[i].InverseTransformPoint(endPt[i]) : Vector3.zero;
+
+      // if (hasTarget[i])
+      //   Debug.Log("Converting world: " + worldOffs + " to local: " + targetLocalOffset[i] + " for " + targetObj[i].name);
     }
 
   } // end of RefreshTargetOffsets
 
   /**
-   * Set an end's target local-offset based on current relative positions.
+   * Update an end's offset to its target based on current relative positions.
    * Use this to update an offset later.
-   */
-  public void RefreshTargetLocalOffset(int end) {
-    target[end].useWorldOffset = false;
-    RefreshTargetOffset(end);
-  }
-
-  /**
-   * Set an end's target world-offset based on current relative positions.
-   * Use this to update an offset later.
-   */
-  public void RefreshTargetWorldOffset(int end) {
-    target[end].useWorldOffset = true;
-    RefreshTargetOffset(end);
-  }
-
-  /**
-   * Set an end's local or world target offset based on current relative positions.
-   * In this implementation, setting one clears the other. But technically it's no
-   * problem to use both in tandem.
    */
   public void RefreshTargetOffset(int end) {
     if (end == 0 || end == 1) {
-      StretchyTarget t = target[end];
-      if (t != null) {
-        if (t.useWorldOffset)
-          t.SetOffsets(Vector3.zero, t.WorldOffsetToPoint(endPoints[end]));
-        else
-          t.SetOffsets(t.LocalOffsetToPoint(endPoints[end]), Vector3.zero);
-      }
+      Transform t = targetObj[end];
+      bool hasTarget = (t != null);
+      // Vector3 worldOffs = endPoints[end] - (hasTarget ? t.position : targetPoint[end]);
+      // targetWorldOffset[end] = worldOffs;
+      targetLocalOffset[end] = hasTarget ? -t.InverseTransformPoint(endPoints[end]) : Vector3.zero;
     }
-  }
-
-  /**
-   * Tether one of the ends to a StretchyTarget (null is ok)
-   */
-  public void TetherEndToTarget(int end, StretchyTarget target) {
-    TetherEndToTargetWithOffset(end, target, Vector3.zero);
   }
 
   /**
    * Tether one of the ends to a Transform (null is ok)
    */
-  public void TetherEndToTransform(int end, Transform trans) {
-    TetherEndToTransformWithOffset(end, trans, Vector3.zero);
-  }
-
-  /**
-   * Tether one of the ends to a StretchyTarget (null is ok) with a world offset
-   */
-  public void TetherEndToTargetWithWorldOffset(int end, StretchyTarget target, Vector3 offset) {
-    TetherEndToTargetWithOffset(end, target, offset, true);
+  public void TetherEndToTransform(int end, Transform target) {
+    if (end == 0 || end == 1)
+      TetherEndToTransformWithOffset(end, target, Vector3.zero);
   }
 
   /**
    * Tether one of the ends to a Transform (null is ok) with a world offset
    */
-  public void TetherEndToTransformWithWorldOffset(int end, Transform trans, Vector3 offset) {
-    TetherEndToTransformWithOffset(end, trans, offset, true);
-  }
-
-  /**
-   * Tether one of the ends to a StretchyTarget (null is ok) with a local offset
-   */
-  public void TetherEndToTargetWithLocalOffset(int end, StretchyTarget target, Vector3 offset) {
-    TetherEndToTargetWithOffset(end, target, offset, false);
-  }
-
-  /**
-   * Tether one of the ends to a Transform (null is ok) with a local offset
-   */
-  public void TetherEndToTransformWithLocalOffset(int end, Transform trans, Vector3 offset) {
-    TetherEndToTransformWithOffset(end, trans, offset, false);
-  }
-
-  /**
-   * Tether one of the ends to a Transform (null is ok) with a local or world offset
-   * The other offset is set to zero, but technically both can be used together
-   */
-  public void TetherEndToTransformWithOffset(int end, Transform trans, Vector3 offset, bool isWorld=false) {
-    GameObject o = trans.gameObject;
-    StretchyTarget t = o.GetComponent<StretchyTarget>();
-    if (t == null) t = o.AddComponent<StretchyTarget>();
-    TetherEndToTargetWithOffset(end, t, offset, isWorld);
-  }
-
-  /**
-   * Tether one of the ends to a StretchyTarget (null is ok) with a local or world offset
-   * The other offset is set to zero, but technically both can be used together
-   */
-  public void TetherEndToTargetWithOffset(int end, StretchyTarget t, Vector3 offset, bool isWorld=false) {
+  public void TetherEndToTransformWithOffset(int end, Transform target, Vector3 offset) {
     if (end == 0 || end == 1) {
-      if (t != null) {
-        if (isWorld)
-          t.SetOffsets(Vector3.zero, offset);
-        else
-          t.SetOffsets(offset, Vector3.zero);
-      }
-      target[end] = t;
+      targetObj[end] = target;
+      // targetWorldOffset[end] = offset;
+      targetLocalOffset[end] = offset;
     }
   }
 
@@ -243,10 +161,11 @@ public class StretchyTethered : Stretchy {
    */
   public void Untether(int end=-1) {
     if (end == -1) {
-      TetherEndToTarget(0, null);
-      end = 1;
+      TetherEndToTransform(0, null);
+      TetherEndToTransform(1, null);
     }
-    TetherEndToTarget(end, null);
+    else
+      TetherEndToTransform(end, null);
   }
 
   /**
@@ -260,14 +179,16 @@ public class StretchyTethered : Stretchy {
   }
 
   /**
-   * Swap target and targetMargin, but not targetPoint
+   * Swap targetObj, targetWorldOffset, targetLocalOffset, and targetMargin, but not targetPoint
    *
    * One Target:
    *  End A now tracks the target.
    *  End B now tracks its last point.
    */
   public void SwapTargetObjects() {
-    target.Swap();
+    targetObj.Swap();
+    targetWorldOffset.Swap();
+    targetLocalOffset.Swap();
     targetMargin.Swap();
   }
 
@@ -276,4 +197,4 @@ public class StretchyTethered : Stretchy {
     SwapTargetObjects();
   }
 
-} // end of StretchyTethered class
+} // class Stretchy
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unity-stretchy
 A simple set of behaviors for Unity 3D which dynamically stretch an object between two points or targets.
+
+The code in this branch is discussed in the article [Stretchy Objects in Unity 3D](http://www.thinkyhead.com/blogs/thinkyhead/2015-11-18/stretchy-objects-unity-3d).
